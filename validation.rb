d3b2e12ed43236c7b4a33896a685d648a4646011
Peter Thoman <petert@dps.uibk.ac.at>
2026-02-20 12:08:22 +0100

    Refactor validation class

--- validation.rb
+++ validation.rb
@@ -1,4 +1,5 @@
 require_relative "general"
+require_relative "validation_class"
 require_relative "validation_helper"
 
 BENCH_SOURCE = "../benchmarks"
@@ -83,37 +84,6 @@ puts "Starting validation:"
 puts "   - experiment results from #{EXPERIMENT_PATH}"
 puts "   - validation builds and outputs in #{VALIDATION_DIR}"
 puts
-
-class ValidationResult
-    attr_accessor :basic_para, :validation_build, :validation_run, :internal_validation, :output_comparison, :err_string
-    def initialize(benchmark, model, par_type, run)
-        @basic_para = false
-        @validation_build = false
-        @validation_run = false
-        @internal_validation = false
-        @output_comparison = false
-        @err_string = ""
-        @benchmark = benchmark
-        @model = model
-        @par_type = par_type
-        @run = run
-    end
-
-    # provide a one-line console summary with emojis for quick overview of which validation steps passed or failed
-    def summary
-        summary_str = "%12s | %10s | %6s | %d : " % [@benchmark, @model, @par_type, @run]
-        summary_str += @basic_para ? "✅ Para " : "❌ Para "
-        summary_str += @validation_build ? "✅ Build " : "❌ Build " if @basic_para
-        summary_str += @validation_run ? "✅ Run " : "❌ Run " if @validation_build
-        summary_str += @internal_validation ? "✅ Validation " : "❌ Validation " if @validation_run
-        summary_str += @output_comparison ? "✅ Comparison" : "❌ Comparison" if @internal_validation
-        return summary_str
-    end
-
-    def is_for(benchmark, model, par_type, run)
-        return @benchmark == benchmark && @model == model && @par_type == par_type && @run == run
-    end
-end
 
 $all_validation_results = []
 
